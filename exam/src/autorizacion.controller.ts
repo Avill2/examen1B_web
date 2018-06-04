df920dbe52ebd5c5fdae1b250ee300ef6399e62f
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-03 22:44:43 -0500

    realizacion de autorizacion controller

--- exam/src/autorizacion.controller.ts
+++ exam/src/autorizacion.controller.ts
@@ -1,7 +1,48 @@
-import {Body, Post} from '@nestjs/common';
+import {Body, Controller, Post, Req, Res} from '@nestjs/common';
 import {APLICACION_SCHEMA} from './Aplicacion/aplicacion.schema';
 import {AplicacacionPipe} from './AplicacionPipe/aplicacacion.pipe';
+import {PeticionErroneaException} from './excepciones/peticion-erronea.exception';
 
+@Controller('Autorizacion')
 export class AutorizacionController{
+    usuario ={
+        usuario: 'AndreaVillacis',
+        password: 1234563006,
+    };
+
+    @Post('iniciarSesion')iniciarSesion(@Req() request, @Res() response, @Body('usuario')usuario:string,@Body('password')password:number){
+        const datos={
+            nombrecookie:'token',
+            valorcookie:this.usuario.usuario,
+        };
+        if (usuario==this.usuario.usuario && this.usuario.password){
+            response.cookie(datos.nombrecookie,datos.valorcookie);
+            return response.send({
+                datos,mensaje:'ok'
+            })
+        }else
+        {
+            throw  new PeticionErroneaException(
+                'No se puede iniciar sesion, los datos son invalidos',10
+            )
+        }
+
+    }
+
+    @Post('cerrarSesion')cerrarSesion(@Req () request, @Res() response){
+        const  datos ={
+            nombrecookie:'token',
+            valorcookie:undefined,
+        };
+        const  existecookie = request.cookies[datos.nombrecookie];
+        if (existecookie){
+            response.cookie(datos.nombrecookie, datos.valorcookie)
+            return response.send({
+                mensaje:'Usted salio del sistema'
+            })
+        }else{
+            return response.status(404).send({mensaje:'no se encontro ninguna cookie'})
+        }
+    }
 
 }
